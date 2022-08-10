180b60293b484942847b97e11ba8726ee45520c3
michael francis <michael_magon@outlook.com>
2022-08-10 23:14:58 +0800

    test commit

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,4 +1,9 @@
 class Service < ApplicationRecord
     has_many :availed_services
     has_many :logs, through: :availed_services
+
+
+    def title_with_price
+        "#{title} - #{price}"
+    end
 end
