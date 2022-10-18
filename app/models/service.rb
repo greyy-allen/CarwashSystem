573df3dbe36aa00b3ba0de892293ffb19a438a13
grey <permale.rey@gmail.com>
2022-10-18 22:28:28 +1000

    form_with with customer and service filter

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,4 +3,8 @@ class Service < ApplicationRecord
     has_many :logs, through: :availed_services
 
     validates :title, :body, :rate, :description, :presence => true
+
+    def self.list
+        self.all.map { |service| [service.title, service.id] }
+    end
 end
